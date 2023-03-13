08075d4f61f2fcda67555f329b2a1cf92d8d685b
Hoang Giang <truonggiang1139@gmail.com>
2023-03-13 18:25:10 +0700

    done

--- src/components/Weather.tsx
+++ src/components/Weather.tsx
@@ -11,51 +11,68 @@ type WeatherType = {
   windSpeed: string;
 };
 export default function Weather() {
-  const [location, setLocation] = useState("");
   const [weatherData, setWeatherData] = useState<WeatherType>();
-  const changeInput = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setLocation(event.target.value);
-  };
-  console.log(weatherData);
+  const inputRef = useRef<HTMLInputElement>(null);
   const checked = (event: React.KeyboardEvent<HTMLInputElement>) => {
     if (event.key === "Enter") {
       search();
     }
   };
-  const inputRef = useRef<HTMLInputElement>(null);
   const search = async () => {
-    setLocation("");
-    inputRef.current?.focus();
-    const res = await axios.get(
-      `https://api.openweathermap.org/data/2.5/weather?q=${location}&lang=vi&appid=${API_key}&units=metric`
-    );
-
-    setWeatherData({
-      city: res.data.name,
-      feels_like: res.data.main.feels_like,
-      temp: res.data.main.temp,
-      humidity: res.data.main.humidity,
-      description: res.data.weather[0].description,
-      windSpeed: res.data.wind.speed,
-    });
+    if (inputRef.current){
+      inputRef.current.focus();
+      let tempCity=inputRef.current.value;
+      inputRef.current.value="";
+      const res = await axios.get(
+        `https://api.openweathermap.org/data/2.5/weather?q=${tempCity}&lang=vi&appid=${API_key}&units=metric`
+      );
+      setWeatherData({
+        city: res.data.name,
+        feels_like: res.data.main.feels_like,
+        temp: res.data.main.temp,
+        humidity: res.data.main.humidity,
+        description: res.data.weather[0].description,
+        windSpeed: res.data.wind.speed,
+      });
+      
+    }
   };
   return (
     <div className="weather_search">
       <div className="weather_search_input">
         <input
           type="text"
-          placeholder="Search a city"
-          value={location}
-          onChange={changeInput}
+          placeholder="Mời nhập địa điểm"
           onKeyDown={checked}
           ref={inputRef}
         />
       </div>
-      <div className="">
+      <div className="main">
         <div className="weather_location">
-          <h3>{weatherData?.city}</h3>
+          <h3>{weatherData?.city?`${weatherData?.city}`:''}</h3>
+        </div>
+        <div className="weather_des">
+          <p>{weatherData?.description?`${weatherData?.description}`:''}</p>
+        </div>
+        <div className="weather_temp">
+          <h1>{weatherData?.temp?`${weatherData?.temp}°C`:''}</h1>
         </div>
       </div>
+      {weatherData && 
+      <div className="footer">
+        <div className="weather_feels_like">
+          <p>Cảm giác như</p>
+          <p>{weatherData?.feels_like?`${weatherData?.feels_like}°C`:''}</p>
+        </div>
+        <div className="weather_humility">
+          <p>Độ ẩm không khí</p>
+          <p>{weatherData?.humidity?`${weatherData?.humidity}%`:''}</p>
+        </div>
+        <div className="weather_wind">
+          <p>Tốc độ gió</p>
+          <p>{weatherData?.windSpeed?`${weatherData?.windSpeed}m/s`:''}</p>
+        </div>
+      </div>}
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -3,18 +3,12 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-const queryClient = new QueryClient();
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <>
-    <QueryClientProvider client={queryClient}>
-      <App />
-      <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>
+    <App/>
   </>
 );
 
